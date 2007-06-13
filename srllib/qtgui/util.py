a7c83a9e06c316422a6a734181a3a9f80fe95376
Arve Knudsen <arvenk@simula.no>
2007-06-13 13:37:01 +0200

    Added a Qt widget BrowseDirectory

--- srllib/qtgui/util.py
+++ srllib/qtgui/util.py
@@ -17,17 +17,8 @@ class BrowseFileButton(QToolButton):
         if tooltip:
             self.setToolTip(tooltip)
 
-class BrowseFile(QWidget):
-    """ Widget composed of a QLineEdit and a L{BrowseFileButton} for browsing the filesystem.
-
-    @cvar DefaultBrowseTooltip: Default tooltip for browse button.
-    @ivar path_edit: QLineEdit for displaying/entering filepath.
-    @ivar: browse_button: L{BrowseFileButton} for opening browse dialog.
-    """
-    DefaultBrowseTooltip = "Browse for file"
-
-    def __init__(self, parent=None, tooltip=None, browse_tooltip=
-            DefaultBrowseTooltip):
+class _Browse(QWidget):
+    def __init__(self, parent, tooltip, browse_tooltip):
         QWidget.__init__(self, parent)
 
         path_edit, browse_btn = self.path_edit, self.browse_button = \
@@ -41,3 +32,60 @@ class BrowseFile(QWidget):
         lay.setMargin(0)
         lay.addWidget(path_edit, 1)
         lay.addWidget(browse_btn, 0, Qt.AlignRight)
+
+        QObject.connect(browse_btn, SIGNAL("clicked()"), self.__slot_browse)
+
+    def __slot_browse(self):
+        fpath = self._get_filepath()
+        if fpath is not None:
+            self.path_edit.setText(fpath)
+
+class BrowseFile(_Browse):
+    """ Widget composed of a QLineEdit and a L{BrowseFileButton} for browsing
+    for a file.
+
+    @cvar DefaultBrowseTooltip: Default tooltip for browse button.
+    @ivar path_edit: QLineEdit for displaying/entering filepath.
+    @ivar: browse_button: L{BrowseFileButton} for opening browse dialog.
+    """
+    DefaultBrowseTooltip = "Browse for file"
+
+    def __init__(self, parent=None, tooltip=None, browse_tooltip=DefaultBrowseTooltip,
+            filter=None):
+        """
+        @param browse_tooltip: Optionally specify tooltip for browse button.
+        @param filter: Optionally specify a file filter (as a string, e.g.,
+        "Images (*.png *.jpg)".
+        """
+        _Browse.__init__(self, parent, tooltip, browse_tooltip)
+
+        self.__filter = filter or QString()
+
+    def _get_filepath(self):
+        fpath = QFileDialog.getOpenFileName(self, "Open File", QString(),
+                self.__filter)
+        if not fpath.isNull():
+            return fpath
+        return None
+
+class BrowseDirectory(_Browse):
+    """ Widget composed of a QLineEdit and a L{BrowseFileButton} for browsing
+    for a directory.
+
+    @cvar DefaultBrowseTooltip: Default tooltip for browse button.
+    @ivar path_edit: QLineEdit for displaying/entering filepath.
+    @ivar: browse_button: L{BrowseFileButton} for opening browse dialog.
+    """
+    DefaultBrowseTooltip = "Browse for directory"
+
+    def __init__(self, parent=None, tooltip=None, browse_tooltip=DefaultBrowseTooltip):
+        """
+        @param browse_tooltip: Optionally specify tooltip for browse button.
+        """
+        _Browse.__init__(self, parent, tooltip, browse_tooltip)
+
+    def _get_filepath(self):
+        fpath = QFileDialog.getExistingDirectory(self, "Open Directory", QString())
+        if not fpath.isNull():
+            return fpath
+        return None
