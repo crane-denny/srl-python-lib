8a201d9d8652d36f5f4ef7fd2809fe02007c86fd
Arve Knudsen <arvenk@simula.no>
2007-07-01 17:03:40 +0200

    Rewrote process module

--- Tests/testprocess.py
+++ Tests/testprocess.py
@@ -1,5 +1,5 @@
 """ Test the signal module. """
-import os.path, time
+import os.path, time, signal
 
 from srllib.testing import *
 import srllib.process as _process
@@ -8,26 +8,44 @@ from _common import *
 class TestError(_process.PickleableException):
     pass
 
+def _childfunc_writes(outstr, errstr):
+    for i in range(10):
+        sys.stdout.write("%s\n" % outstr)
+        sys.stderr.write("%s\n" % errstr)
+    
+def _childfunc_raises():
+        raise TestError("TestError")
+
+def _childfunc_sleeps():
+    import time
+    while True:
+        time.sleep(0.1)
+
 class ProcessTest(TestCase):
+    
     def test_child_exception(self):
         """ Test catching an exception raised in the child. """
-        
-        def childfunc(process):
-            raise TestError("Error in child")
-        proc = _process.Process(childfunc)
+        proc = _process.Process(_childfunc_raises)
         try: proc.wait()
-        except _process.ChildError, err:            
+        except _process.ChildError, err:
             self.assert_(isinstance(err.orig_exception, TestError))
-                
+        else:
+            print proc.stderr.read()
+            raise AssertionError, "Exception not raised"
+        # Polling should raise the same error
+        self.assertRaises(_process.ChildError, proc.poll)
+        proc.close()
+        
+    def test_terminate(self):
+        """ Test terminating the child process. """
+        proc = _process.Process(_childfunc_sleeps)
+        self.assertEqual(proc.terminate(), -signal.SIGTERM)
+        # Make sure that it is safe to call this after the process has exited
+        self.assertEqual(proc.terminate(), -signal.SIGTERM)
+    
+    '''
     def test_run_in_terminal(self):
         """ Test running code in virtual terminal. """
-        import time
-        def childfunc(prcs, outstr, errstr):
-            import sys
-            for i in range(10):
-                sys.stdout.write("%s\n" % outstr)
-                sys.stderr.write("%s\n" % errstr)
-
         if get_os_name() == Os_Linux:
             # Execute child function under supervision and observe outputs
             def slot_stdout(txt):
@@ -40,16 +58,24 @@ class ProcessTest(TestCase):
             self._connect_to(procmon.sig_stdout, slot_stdout)
             self._connect_to(procmon.sig_stderr, slot_stderr)
             
-            procmon(childfunc, ["Test out", "Test err"])
+            procmon(_childfunc_writes, ["Test out", "Test err"])
             procmon.wait()
 
             for l in self.__stdout.splitlines():
                 self.assertEqual(l, "Test out")
             for l in self.__stderr.splitlines():
                 self.assertEqual(l, "Test err")
+    '''
+       
+def _childfunc_succeeds():
+    pass  
+
+def _childfunc_fails():
+    raise Exception("Failure")     
                 
 class ThreadedProcessMonitorTest(TestCase):
     """ Test the threaded process monitor. """
+    '''
     def test_capture_output(self):
         """ Test capturing textual output from child process. """
         def slot_stdout(text):
@@ -70,18 +96,39 @@ class ThreadedProcessMonitorTest(TestCase):
         procmon.wait()
         self.assertEqual(self.__stdout, "Test stdout")
         self.assertEqual(self.__stderr, "Test stderr")
+    '''
     
-    def test_terminate(self):
-        """ Test terminating the monitored process. """
+    def test_success(self):
+        """ Verify that sig_finished is received when the process finishes
+        successfully. """
         def slot_finished():
             self.__finished = True
-        def childfunc(process):
-            while True:
-                time.sleep(0.1)
+        def slot_failed(err):
+            self.__failed = True
+        
+        procmon = _process.ThreadedProcessMonitor()
+        self.__finished = self.__failed = False
+        self._connect_to(procmon.sig_finished, slot_finished)
+        self._connect_to(procmon.sig_failed, slot_failed)
+        procmon(_childfunc_succeeds)
+        procmon.wait()
+        self.assert_(self.__finished)
+        self.assertNot(self.__failed)
+        
+    def test_failure(self):
+        """ Verify that sig_failed is received when the process fails. """
+        def slot_finished():
+            self.__finished = True
+        def slot_failed(err):
+            self.__error = err
         
         procmon = _process.ThreadedProcessMonitor()
         self.__finished = False
+        self.__error = None
         self._connect_to(procmon.sig_finished, slot_finished)
-        procmon(childfunc)
-        procmon.terminate_process(wait=True)
-        self.assert_(self.__finished)
+        self._connect_to(procmon.sig_failed, slot_failed)
+        procmon(_childfunc_fails)
+        procmon.wait()
+        self.assert_(isinstance(self.__error, _process.ChildError), self.__error)
+        self.assertNot(self.__finished)
+    
