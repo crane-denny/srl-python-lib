912ef4832a73094161077efc0556e298ee0b59c6
Arve Knudsen <arvenk@simula.no>
2007-09-18 12:02:43 +0200

    Polish

---
+++ Tests/_common.py
@@ -0,0 +1,2 @@
+from srllib.util import get_os, get_os_name, get_os_version, Os_Linux, Os_Windows
+from srllib.testing import *
---
+++ srllib/qtgui/_common.py
@@ -0,0 +1,20 @@
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+def message_critical(title, text, detailedText=None, informativeText=None, parent=None):
+    """ Display a critical message in a L{QMessageBox}. """
+    dlg = QMessageBox(QMessageBox.Critical, title, text, QMessageBox.Ok, parent)
+    if informativeText:
+        dlg.setInformativeText(informativeText)
+    if detailedText:
+        dlg.setDetailedText(detailedText)
+    dlg.exec_()
+
+def message_warning(title, text, detailedText=None, informativeText=None, parent=None):
+    """ Display a warning message in a L{QMessageBox}. """
+    dlg = QMessageBox(QMessageBox.Warning, title, text, QMessageBox.Ok, parent)
+    if informativeText:
+        dlg.setInformativeText(informativeText)
+    if detailedText:
+        dlg.setDetailedText(detailedText)
+    dlg.exec_()
