02a1d920cb8650ab2711a6c6e7164d489215ead8
Arve Knudsen <arvenk@simula.no>
2007-10-07 17:27:16 +0200

    Add class QtTestCase which replaces Qt's signal-slot mechanism with its own

--- srllib/testing/qtgui/mock.py
+++ srllib/testing/qtgui/mock.py
@@ -5,6 +5,23 @@ from srllib.testing.mock import *
 class QMock(QObject, Mock):
     """ Mock class that also inherits from QObject.
     """
+    __connections = {}
+    
     def __init__(self):
         QObject.__init__(self)
         Mock.__init__(self)
+        
+    @classmethod
+    def connect(cls, sender, signal, slot):
+        cls.__connections.setdefault((sender, signal), set()).add(slot)
+        
+    @classmethod
+    def mock_clear_connections(cls):
+        cls.__connections.clear()
+        
+    def emit(self, signal, *args):
+        print "Emitting", self.__connections
+        for slot in self.__connections.get((self, signal), []):
+            print "Calling slot %r" % slot
+            slot(*args)
+            
