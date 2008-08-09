11d34fa2bd6e52eb9b179860adeed5936324ff38
Arve Knudsen <arvenk@simula.no>
2008-08-09 12:33:35 +0200

    Add UndoStack to qtgui

--- Tests/testqtgui/_common.py
+++ Tests/testqtgui/_common.py
@@ -1,3 +1,6 @@
+from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import Qt
+
 from srllib.util import (get_os, get_os_name, get_os_version, Os_Linux,
     Os_Windows)
 from srllib.testing.qtgui import *
