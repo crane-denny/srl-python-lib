0d19ab1687c809e607131db85a73d6e772a3fe74
Arve Knudsen <arvenk@simula.no>
2008-08-19 10:26:24 +0200

    Fix casing in some function arguments

--- srllib/qtgui/_common.py
+++ srllib/qtgui/_common.py
@@ -3,22 +3,22 @@ from PyQt4.QtCore import Qt
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
-def message_critical(title, text, detailedText=None, informativeText=None,
+def message_critical(title, text, detailed_text=None, informative_text=None,
     parent=None):
     """ Display a critical message in a L{QMessageBox}. """
     dlg = QMessageBox(QMessageBox.Critical, title, text, QMessageBox.Ok, parent)
-    if informativeText:
-        dlg.setInformativeText(informativeText)
-    if detailedText:
-        dlg.setDetailedText(detailedText)
+    if informative_text:
+        dlg.setInformativeText(informative_text)
+    if detailed_text:
+        dlg.setDetailedText(detailed_text)
     dlg.exec_()
 
-def message_warning(title, text, detailedText=None, informativeText=None,
+def message_warning(title, text, detailed_text=None, informative_text=None,
     parent=None):
     """ Display a warning message in a L{QMessageBox}. """
     dlg = QMessageBox(QMessageBox.Warning, title, text, QMessageBox.Ok, parent)
-    if informativeText:
-        dlg.setInformativeText(informativeText)
-    if detailedText:
-        dlg.setDetailedText(detailedText)
+    if informative_text:
+        dlg.setInformativeText(informative_text)
+    if detailed_text:
+        dlg.setDetailedText(detailed_text)
     dlg.exec_()
