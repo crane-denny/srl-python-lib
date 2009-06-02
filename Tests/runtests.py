39f4fb7b80fb72d3aec471bcf751956f693e2035
Arve Knudsen <arvenk@simula.no>
2009-06-02 18:11:39 +0200

    Allow running tests from any directory

--- Tests/runtests.py
+++ Tests/runtests.py
@@ -2,6 +2,10 @@
 
 import sys, os.path
 
+dpath = os.path.dirname(__file__)
+if dpath:
+    os.chdir(dpath)
+
 sys.path.insert(0, os.path.pardir)
 import srllib.testing
 srllib.testing.run_tests("srllib")
