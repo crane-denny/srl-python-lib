ff0969308366f36d17d5cc0b6fdb063bc1af502d
Arve Knudsen <arve.knudsen@gmail.com>
2011-04-18 15:40:24 +0200

    Update distribution metadata

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ from setuptools import setup, find_packages
 import srllib
 
 extra_kwds = {}
-setup(name="SRL Python Library",
+setup(name="srllib",
         version=srllib.__version__,
-        author="Simula Research Laboratory",
-        author_email="arvenk@simula.no",
+        author="Arve Knudsen",
+        author_email="arve.knudsen@gmail.com",
         url="http://code.google.com/p/srl-python-lib",
         license=srllib.__license__,
         packages=find_packages(exclude=["Tests"]),
